059d4655afd5307d6fe953596abdca4cf65679ab
karunon <54615644+karunon@users.noreply.github.com>
2025-03-04 00:55:50 +0900

    feat(header): add nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,13 @@ export default () => {
       <header className="grid m-4 gap-2">
         <p className="text-2xl font-bold">Karunon's blog</p>
         <p className="text-gray-600">Designer</p>
+        <nav>
+          <ul className="flex w-fit gap-4 rounded-full border border-gray-700 px-4 py-2">
+            <li>
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
