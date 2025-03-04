8e8749379b1611c943a9f24a0fb7f474d4be5333
karunon <54615644+karunon@users.noreply.github.com>
2025-03-05 02:13:24 +0900

    refactor(Header): extract sticky nav out of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 export default () => {
   return (
     <>
-      <header className="grid m-4 gap-2">
+      <header className="grid p-4 gap-2">
         <p className="text-2xl font-bold">Karunon's blog</p>
         <p className="text-gray-600">Designer</p>
-        <nav>
+      </header>
+      <nav className="sticky top-1 px-4 pb-4">
           <ul className="flex w-fit gap-4 rounded-full border border-gray-700 px-4 py-2">
             <li>
               <a href="/">Home</a>
             </li>
           </ul>
         </nav>
-      </header>
     </>
   );
 };
