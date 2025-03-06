f665a1bc2f23bb8c87ba25cc7f57d0ca0e741fe9
karunon <54615644+karunon@users.noreply.github.com>
2025-03-06 22:38:50 +0900

    style: apply code formatter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export default () => {
         <p className="text-gray-600">Designer</p>
       </header>
       <nav className="sticky top-1 px-4 pb-4">
-          <ul className="flex w-fit gap-4 rounded-full border border-gray-700 px-4 py-2">
-            <li>
-              <a href="/">Home</a>
-            </li>
-          </ul>
-        </nav>
+        <ul className="flex w-fit gap-4 rounded-full border border-gray-700 px-4 py-2">
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
